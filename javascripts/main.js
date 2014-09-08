1792b95c78352b847a5d5ae3ddb9ba26320fb979
necaimi <308048801@qq.com>
2014-09-08 19:38:33 +0800

    2014/09/08 19:38

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,34 @@
 
-define( function(require){
+require.config({
     
-    var newJS = require("text!extension/ext.json");
-        console.debug(newJS);
+        baseUrl:"javascripts",
+        paths:{
+            jQuery:"jquery-1.11.1.min",
+        }
+    
+    });
+
+define(["domReady!", "jQuery"], function(domReady, $, require){
+    
+    //init form size;
+    var winWidth, winHeight;
+    console.debug($);
+    
+   // var $navbox = $("[com-type='navBox']"), $toolbox = $("[com-type='toolBox']"), $editbox = $("[com-type='editBox']");
+    
+    function InitForm()
+    {
+        
+    if(document.documentElement)
+    {
+        winWidth = document.documentElement.clientWidth;
+        winHeight = document.documentElement.clientHeight;
+        
+       // $editbox.height = $toolbox.height = winHeight - $navbox.height;
+    }
+        
+    };
+    
+        InitForm();
     
     });
