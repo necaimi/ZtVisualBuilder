25b49e38640b50880cadb2236462923c9d9c5180
necaimi <308048801@qq.com>
2014-09-10 17:42:54 +0800

    last sss

--- extensions/modulebuilder/main.js
+++ extensions/modulebuilder/main.js
@@ -3,5 +3,15 @@ define(function(require, exports, module){
     
     console.debug("module builder init");
     
+    var ToolDialogHTML = require("text!extensions/modulebuilder/htmlmodule/tooldialog.html");
     
+    var UiText = {"Title":"模板生成工具"};
+    
+    console.debug(Mustache.render);
+    
+    var ToolDialogTemplate = Mustache.render(ToolDialogTemplate, "{UI:UiText}");
+    
+    
+    
+    $("#main-dialog").append($(ToolDialogTemplate));
 });
