03b8511727f707c56092031807f0941615d94f60
necaimi <308048801@qq.com>
2014-09-30 17:50:32 +0800

    dialog 1750

--- module/dialog/Dialogs.js
+++ module/dialog/Dialogs.js
@@ -10,17 +10,55 @@ define(function(require, exports, module){
     
     var z_index = 1000;    
     
-    function Dialog($dlg){
-        this._$dlg = $dlg;
+    function Dialog($dlg, b_modal){
+        this._$dlg = $dlg,
+        this._candrag = b_modal,
+        this._titleBar = null,
+        this._srcWidth = $dlg.width,
+        this._srcHeight = $dlg.height;
     };
     
-    Dialog.prototype.getDlgObject(){
+    Dialog.prototype.GetDlgObject(){
         return this._$dlg;
     };
     
+    Dialog.prototype.SetMaxSize(){
+        var max_width = $("body").width,
+            max_height= $("body").height;
+        
+        this._$dlg.stop(false, true).animate({"width":max_width, "height":max_height}, 300);
+        
+    };
+    
+    Dialog.prototype.SetNormalSize(){
+          this._$dlg.stop(false, true).animate({"width":this._srcWidth, "height":this._srcHeight}, 300);
+    };
     
     
-    function ShowDialog(template){
+    Dialog.prototype.SetMinSize(){
+        if(!this._titleBar){
+            return null;
+        }
+        
+        var t_bar_width = this._titleBar.width,
+            t_bar_height = this._titleBar.height;
+        
+        
+    };
+    
+    Dialog.prototype.Close(){
+        
+    };
+    
+    function TitleBar(){
+        this._candrag = true;
+    };
+    
+    TitleBar.prototype.DragEnabled(b){
+        this._candrag = b;
+    };
+    
+    function ShowDialog(template, b_modal){
         if(!template){
             console.error("no template");
             return null;
@@ -33,7 +71,7 @@ define(function(require, exports, module){
                    .appendTo(".dlg-inner:last");
         
         
-        return (new Dialog($dlg));
+        return (new Dialog($dlg, b_modal));
     };
     
     exports.ShowDialog = ShowDialog;
